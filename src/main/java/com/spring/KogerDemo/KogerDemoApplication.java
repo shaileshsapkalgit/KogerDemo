55db189cd12f0f43484699b190c14c9466ad46ca
Shailesh Sapkal <shaileshsapkal1829@gmail.com>
2025-06-04 13:58:38 +0530

    Initial commit

---
+++ src/main/java/com/spring/KogerDemo/KogerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.KogerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KogerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KogerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/KogerDemo/KogerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.KogerDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KogerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
